a076780ef3f839630958b79d2d58a63b7d81d4ad
wailin-tw <wailin.s@trinitywizards.com>
2026-04-30 10:25:47 +0800

    updated helm setup verion in GHA

--- main.tf
+++ main.tf
@@ -522,21 +522,21 @@ resource "oci_core_network_security_group_security_rule" "nsg_prod_airs_ingress_
     }
   }
 }
-resource "oci_core_network_security_group_security_rule" "nsg_prod_airs_ingress_lb" {
-  network_security_group_id = oci_core_network_security_group.nsg_prod_airs.id
-  direction                 = "INGRESS"
-  protocol                  = "6"
-  # source                    = "10.10.0.0/24"
-  source = var.lb_subnet_cidr
-  source_type               = "CIDR_BLOCK"
-  description               = "Allow OCI load balancer or network load balancer to communicate with kube-proxy on worker nodes."
-  tcp_options {
-    destination_port_range {
-      min = 10256
-      max = 10256
-    }
-  }
-}
+# resource "oci_core_network_security_group_security_rule" "nsg_prod_airs_ingress_lb" {
+#   network_security_group_id = oci_core_network_security_group.nsg_prod_airs.id
+#   direction                 = "INGRESS"
+#   protocol                  = "6"
+#   # source                    = "10.10.0.0/24"
+#   source = var.lb_subnet_cidr
+#   source_type               = "CIDR_BLOCK"
+#   description               = "Allow OCI load balancer or network load balancer to communicate with kube-proxy on worker nodes."
+#   tcp_options {
+#     destination_port_range {
+#       min = 10256
+#       max = 10256
+#     }
+#   }
+# }
 resource "oci_core_network_security_group_security_rule" "nsg_prod_airs_ingress_icmp_from_bastion" {
   network_security_group_id = oci_core_network_security_group.nsg_prod_airs.id
   direction                 = "INGRESS"
