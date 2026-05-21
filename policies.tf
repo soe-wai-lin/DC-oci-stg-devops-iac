20d22a345a081fa6b3826d09ade35c09b1f6043c
wailin-tw <wailin.s@trinitywizards.com>
2026-05-21 12:03:41 +0800

    added to read secret-bundle for vault secret retrieve

--- policies.tf
+++ policies.tf
@@ -138,25 +138,25 @@ resource "oci_identity_policy" "dg_for_retrieve_vault_secret" {
   statements = [
     "Allow dynamic-group ${oci_identity_dynamic_group.dg_for_retrieve_vault_secret.name} to use secret-family in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq",
     "Allow dynamic-group ${oci_identity_dynamic_group.dg_for_retrieve_vault_secret.name} to use keys in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq",
-    "Allow dynamic-group ${oci_identity_dynamic_group.dg_for_retrieve_vault_secret.name} to read vaults in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq"
-  
+    "Allow dynamic-group ${oci_identity_dynamic_group.dg_for_retrieve_vault_secret.name} to read vaults in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq",
+    "Allow dynamic-group ${oci_identity_dynamic_group.dg_for_retrieve_vault_secret.name} to read secret-bundles in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq"  
   ]
 }
-resource "oci_identity_policy" "workload_identity_vault_access" {
-  provider       = oci.home
-  compartment_id = var.tenancy_ocid
+# resource "oci_identity_policy" "workload_identity_vault_access" {
+#   provider       = oci.home
+#   compartment_id = var.tenancy_ocid
 
-  name        = "${var.airs_cluster_name}-workload-secret-policy"
-  description = "Allow OKE workload identity to access vault secrets"
+#   name        = "${var.airs_cluster_name}-workload-secret-policy"
+#   description = "Allow OKE workload identity to access vault secrets"
 
-  statements = [
-    "Allow any-user to use secret-family in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq where ALL {request.principal.type='workload', request.principal.namespace='secrets-store-oci', request.principal.service_account='oci-secrets-store-csi-driver-provider-sa', request.principal.cluster_id='${oci_containerengine_cluster.stg_oke.id}'}",
+#   statements = [
+#     "Allow any-user to use secret-family in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq where ALL {request.principal.type='workload', request.principal.namespace='secrets-store-oci', request.principal.service_account='oci-secrets-store-csi-driver-provider-sa', request.principal.cluster_id='${oci_containerengine_cluster.stg_oke.id}'}",
 
-    "Allow any-user to use vaults in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq where ALL {request.principal.type='workload', request.principal.namespace='secrets-store-oci', request.principal.service_account='oci-secrets-store-csi-driver-provider-sa', request.principal.cluster_id='${oci_containerengine_cluster.stg_oke.id}'}",
+#     "Allow any-user to use vaults in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq where ALL {request.principal.type='workload', request.principal.namespace='secrets-store-oci', request.principal.service_account='oci-secrets-store-csi-driver-provider-sa', request.principal.cluster_id='${oci_containerengine_cluster.stg_oke.id}'}",
 
-    "Allow any-user to use keys in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq where ALL {request.principal.type='workload', request.principal.namespace='secrets-store-oci', request.principal.service_account='oci-secrets-store-csi-driver-provider-sa', request.principal.cluster_id='${oci_containerengine_cluster.stg_oke.id}'}"
-  ]
-}
+#     "Allow any-user to use keys in compartment id ocid1.compartment.oc1..aaaaaaaaurbvoggxsyrbvdw7oscscre64rcdd7daetbvdc5ftuolzvxmxbsq where ALL {request.principal.type='workload', request.principal.namespace='secrets-store-oci', request.principal.service_account='oci-secrets-store-csi-driver-provider-sa', request.principal.cluster_id='${oci_containerengine_cluster.stg_oke.id}'}"
+#   ]
+# }
 
 
 
