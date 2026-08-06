f1099860bc1a220d9dd04f2ab17e1ec1a6c5db42
wailin-tw <wailin.s@trinitywizards.com>
2026-08-07 00:04:21 +0800

    allowed 3100 from gf to lb

--- main.tf
+++ main.tf
@@ -402,6 +402,24 @@ resource "oci_core_network_security_group_security_rule" "nsg_prod_lb_ingress_pr
   }
 }
 
+resource "oci_core_network_security_group_security_rule" "nsg_prod_lb_ingress_loki_3100" {
+  network_security_group_id = oci_core_network_security_group.nsg_prod_lb.id
+  direction                 = "INGRESS"
+  protocol                  = "6"
+  # source                    = "0.0.0.0/0"
+  source                    = var.lb_subnet_cidr
+  source_type               = "CIDR_BLOCK"
+  description               = "Allow loki port to Load Balancer."
+
+  # Optional: Restrict to ping only (echo request = type 8)
+  tcp_options {
+    destination_port_range {
+      min = 3100
+      max = 3100
+    }
+  }
+}
+
 # For multiple egress target , create "local" data
 
 locals {
